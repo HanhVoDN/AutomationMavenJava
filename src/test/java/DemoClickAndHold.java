8f8b3e3ce1871be1e3a917145426bf3ca9aa1f67
LAPTOP-FDL8NHPL\bichhanh <bichhanh.vodn@gmail.com>
2023-11-13 09:40:30 -0600

    Merge remote-tracking branch 'origin/master'

---
+++ src/test/java/DemoClickAndHold.java
@@ -0,0 +1,37 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.concurrent.TimeUnit;
+
+public class DemoClickAndHold {
+    static WebDriver driver;
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+        driver.navigate().to("https://demoqa.com/droppable");
+
+        // Identify the elements to perform clickAndHold operation
+        WebElement source = driver.findElement(By.id("draggable"));
+        WebElement target = driver.findElement(By.id("droppable"));
+
+        // Create Actions class instance
+        Actions builder = new Actions(driver);
+
+        // Click and hold the source element, then move to the target and release
+        builder.clickAndHold(source).moveToElement(target).release().build().perform();
+
+
+        // Close the browser
+        //driver.quit();
+    }
+
+}
