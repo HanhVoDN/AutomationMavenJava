115cf63a1e0cd1cf6dbae572b660d24052d3ea5e
LAPTOP-FDL8NHPL\bichhanh <bichhanh.vodn@gmail.com>
2023-11-14 01:10:39 -0600

    Merge remote-tracking branch 'origin/master'

---
+++ src/test/java/DemoContextClick.java
@@ -0,0 +1,35 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.concurrent.TimeUnit;
+
+public class DemoContextClick {
+    static WebDriver driver;
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+        driver.navigate().to("https://demoqa.com/buttons");
+
+        // Identify the element
+        WebElement element = driver.findElement(By.id("rightClickBtn"));
+
+        // Create Actions class instance
+        Actions builder = new Actions(driver);
+
+        // Click right mouse on the element
+        builder.contextClick(element).perform();
+
+        // Close the browser
+        //driver.quit();
+    }
+
+}
