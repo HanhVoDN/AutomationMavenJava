74db3de58d3561037a8f27cf30abaf56636b5a94
LAPTOP-FDL8NHPL\bichhanh <bichhanh.vodn@gmail.com>
2023-11-14 23:55:58 -0600

    Merge remote-tracking branch 'origin/master'

---
+++ src/test/java/DemoScrollByAmoit.java
@@ -0,0 +1,35 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.concurrent.TimeUnit;
+
+public class DemoScrollByAmoit {
+    static WebDriver driver;
+    public static <JavascriptExecutor> void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+        driver.navigate().to("https://demoqa.com/text-box");
+
+        // Identify the element to which you want to scroll
+        WebElement elementToScroll = driver.findElement(By.xpath("//div[text()='Interactions']"));
+
+        // Scroll to the element
+        Actions actions=new Actions(driver);
+        actions.scrollByAmount(300,800);
+
+        // Other actions with the scrolled-to element (e.g., click, interact, etc.)
+        elementToScroll.click();
+
+
+        // Close the browser
+        //driver.quit();
+    }
+
+}
