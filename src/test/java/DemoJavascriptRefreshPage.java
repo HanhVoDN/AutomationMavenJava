a0923b47db9e3ffacfed7e12ba0a77dcfe098d53
LAPTOP-FDL8NHPL\bichhanh <bichhanh.vodn@gmail.com>
2023-11-16 02:32:44 -0600

    Merge remote-tracking branch 'origin/master'

---
+++ src/test/java/DemoJavascriptRefreshPage.java
@@ -0,0 +1,31 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+public class DemoJavascriptRefreshPage {
+    static WebDriver driver;
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+        driver.navigate().to("https://demoqa.com/droppable");
+
+        // Refresh the page using JavaScript
+        refreshPageWithJavaScript((JavascriptExecutor) driver);
+
+        // Close the browser
+        // driver.quit();
+}
+    private static void refreshPageWithJavaScript(JavascriptExecutor jsExecutor) {
+        jsExecutor.executeScript("history.go(0)");
+    }
+}
